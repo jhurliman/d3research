4c0068f8435186782c4a1ebc1c820b86cd8c9512
John Hurliman <jhurliman@jhurliman.org>
2012-01-07 03:56:44 -0800

    Determine actor types, resolve a GUID to an RActor, and fetch the player GUID

--- DotNet/d3sandbox/d3sandbox/Program.cs
+++ DotNet/d3sandbox/d3sandbox/Program.cs
@@ -7,6 +7,40 @@ namespace d3sandbox
 {
     class Program
     {
+        static uint GetPlayerGUID(BlackMagic d3, uint pObjMgr)
+        {
+            const uint INVALID = 0xFFFFFFFF;
+
+            //v0 = *(gpObjMgr + 2352);
+            //if (*v0 != -1 && (v1 = *(getObjMgrPlus1916() + 168)) != 0 && (v2 = 30520 * *v0 + v1 + 88) != 0)
+            //    result = *(v2 + 8);
+
+            uint v0 = d3.ReadUInt(d3.ReadUInt(pObjMgr + 2352));
+            if (v0 == INVALID)
+                return INVALID;
+
+            uint v1 = d3.ReadUInt(pObjMgr + 1916 + 168);
+            if (v1 == 0)
+                return INVALID;
+
+            uint v2 = 30520 * v0 + v1 + 88;
+            if (v2 == 0)
+                return INVALID;
+
+            return d3.ReadUInt(v2 + 8);
+        }
+
+        static RActor GetRActorFromGUID(BlackMagic d3, uint pRActors, uint guid)
+        {
+            uint v0 = d3.ReadUInt(pRActors + 328);
+            int v1 = d3.ReadInt(pRActors + 396);
+
+            uint ptr = d3.ReadUInt(v0 + 4 * ((guid & 0xFFFF) >> v1)) + 1068 * (uint)(guid & ((1 << v1) - 1));
+            if (d3.ReadUInt(ptr) == guid)
+                return GetRActor(d3, ptr);
+            return null;
+        }
+
         static RActor GetRActor(BlackMagic d3, uint ptr)
         {
             if (d3.ReadUInt(ptr) == 0xFFFFFFFF)
@@ -17,13 +51,21 @@ namespace d3sandbox
 
         static List<RActor> GetRActors(BlackMagic d3)
         {
+            // Fetch the global pointer to the object manager
+            uint pObjMgr = d3.ReadUInt(0x140593C);
+
             // Fetch the the global pointer to the RActors container
-            uint pRActors = d3.ReadUInt(d3.ReadUInt(0x140593C) + 0x8AC);
+            uint pRActors = d3.ReadUInt(pObjMgr + 0x8AC);
             if (d3.ReadASCIIString(pRActors, 7) != "RActors")
                 throw new InvalidHostException("Cannot find RActors");
 
             // Grab the size of the RActors array
             int actorArraySize = d3.ReadInt(pRActors + 268);
+
+            // Get the player actor
+            uint playerGuid = GetPlayerGUID(d3, pObjMgr);
+            RActor playerActor = GetRActorFromGUID(d3, pRActors, playerGuid);
+            Console.WriteLine("Player actor: " + playerActor);
 
             // Grab the first RActor
             uint pRActor = d3.ReadUInt(d3.ReadUInt(pRActors + 328));
--- DotNet/d3sandbox/d3sandbox/Types.cs
+++ DotNet/d3sandbox/d3sandbox/Types.cs
@@ -41,31 +41,52 @@ namespace d3sandbox
         }
     }
 
+    public enum ActorType
+    {
+        Other,
+        Environment,
+        ACDBased
+    }
+
     public class RActor
     {
         public uint Guid;
-        public uint Unk1;
+        public int Unk1;
         public string ModelName;
         public Vector3 Dir;
         public Vector3 Pos1;
         public float Unk2;
         public Vector3 Pos2;
+        public int Unk3;
+
+        public ActorType Type
+        {
+            get
+            {
+                if (Unk1 != -1)
+                    return ActorType.ACDBased;
+                if (Unk3 == -1)
+                    return ActorType.Other;
+                return ActorType.Environment;
+            }
+        }
 
         public RActor(byte[] data)
         {
             this.Guid = BitConverter.ToUInt32(data, 0);
-            this.Unk1 = BitConverter.ToUInt32(data, 4);
+            this.Unk1 = BitConverter.ToInt32(data, 4);
             this.ModelName = Utils.AsciiBytesToString(data, 8, 140);
             this.Dir = new Vector3(data, 148);
             this.Pos1 = new Vector3(data, 160);
             this.Unk2 = BitConverter.ToSingle(data, 172);
             this.Pos2 = new Vector3(data, 176);
+            this.Unk3 = BitConverter.ToInt32(data, 344);
         }
 
         public override string ToString()
         {
-            return String.Format("GUID={0:X}, Unk1={1}, Model={2}, Dir={3}, Pos1={4}, Unk2={5}, Pos2={6}",
-                Guid, Utils.BitsToString(Unk1), ModelName, Dir, Pos1, Unk2, Pos2);
+            return String.Format("[{8}] GUID={0:X}, Unk1={1:X}, Model={2}, Dir={3}, Pos1={4}, Unk2={5}, Pos2={6}, Unk3={7}",
+                Guid, Unk1, ModelName, Dir, Pos1, Unk2, Pos2, Unk3, Type);
         }
     }
 }
